90b2c44881fde9421082815d92c29b26d711f0a3
Nikhil6 Patel <nikhil6.patel@ril.com>
2023-05-26 00:30:40 +0530

    Updating default font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,10 @@
 import './globals.css'
-import { Inter } from 'next/font/google'
+import { Poppins } from 'next/font/google'
 import LayoutWrapper from '@/components/Layout/LayoutWrapper'
 import { WithChildren } from '@/types/common'
 import ThemeProvider from '@/components/ThemeProvider'
 
-const inter = Inter({ subsets: ['latin'] })
+const poppins = Poppins({ subsets: ['latin'], weight : ["400"] })
 
 export const metadata = {
   title: 'Create Next App',
@@ -14,7 +14,7 @@ export const metadata = {
 export default function RootLayout(props: WithChildren) {
   const { children } = props
   return (
-    <html lang="en" className={inter.className}>
+    <html lang="en" className={poppins.className}>
       <body>
         <ThemeProvider attribute="class" defaultTheme='system' enableSystem>
           <LayoutWrapper>
